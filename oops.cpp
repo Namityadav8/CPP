ebbcf7a0a754b0bde8e733d3ae4366a20bb3a09b
Namit <namityadav008@gmail.com>
2023-08-14 21:11:03 +0530

    updates

--- oops.cpp
+++ oops.cpp
@@ -1,12 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-class namit{
+class Student {
+    private:
+        string name;
+        string course;
+        int marks;
     public:
-    int age=19;
-
+        void setName(string newName) {
+            name = newName;
+        }
+         void setCourse(string newCourse) {
+            course = newCourse;
+        }
+        // adding validation 
+        void setMarks(int newMarks){
+     if (newMarks < 0) {
+     throw "Marks cannot be negative !!";
+}
+     //Set the value of the new marks
+     marks = newMarks;
+       }
+        void Student_Info() {
+            cout << "Name: " << name << endl;
+            cout << "Course: " << course << endl;
+            cout << "Marks: " << marks << endl;
+        }
 };
-
-int main(){
-    namit yadav;
-    cout<<"my age is"<<" "<<yadav.age<<endl;
+int main() {
+    Student obj;
+    obj.setName("Ninja Coder");
+    obj.setMarks(99);
+    obj.setCourse("DSA in C++");
+    obj.Student_Info();
+    return 0;
 }
