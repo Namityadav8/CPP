c2bf7de1980d823f6555afc16e1478eb439de650
Namit <namityadav008@gmail.com>
2023-09-30 12:49:49 +0530

    updates

---
+++ Switch Case .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=3;
+    switch(n){
+        case 1:
+        cout<<"nope";
+        break;
+
+        case 3:
+        cout<<"ha yahi h";
+        break;
+    }
+}
