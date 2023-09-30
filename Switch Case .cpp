8292bea5a354c19a1402ef0134db4c8f4907761d
Namit <namityadav008@gmail.com>
2023-09-30 13:06:43 +0530

    updates

--- Switch Case .cpp
+++ Switch Case .cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main(){
     int n=3;
+    int a=3;
     switch(n){
         case 1:
         cout<<"nope";
         break;
 
         case 3:
+        switch(a){
+            case 3:
+            cout<<"The value of a is "<<a<<endl;
+        }
         cout<<"ha yahi h";
         break;
     }
