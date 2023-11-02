cf29707c4adbe0abeae4b708e76b7c8d0907a295
Namit <namityadav008@gmail.com>
2023-11-03 01:38:33 +0530

    changes

--- insertion sort .cpp
+++ insertion sort .cpp
@@ -1,4 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std; 
+void insertionsort(int n,vector<int> &arr){
+    
+    for(int i=1;i<n;i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(j;j<=0;j--){
+            if(arr[j]>temp){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+            arr[j+1] = temp;
+        }
+    }
+}
 int main(){
 } 
